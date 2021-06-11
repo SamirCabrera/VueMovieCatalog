07ec459b5f512b9ff2b1c4a618bbb7563ce4da5f
Samir <samircm1406@gmail.com>
2021-06-11 19:12:05 +0200

    ultimo commit

--- src/main.js
+++ src/main.js
@@ -6,12 +6,24 @@ import Vuelidate from 'vuelidate'
 import { store } from './store/store'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
+import firebase from "firebase/app";
 
 
 Vue.config.productionTip = false
 
 Vue.use(VueMaterial)
 Vue.use(Vuelidate)
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBmU6xWas9-Zd2YOrg7Rj08xMSNk19RWTw",
+  authDomain: "moviecatalog-5b6c8.firebaseapp.com",
+  projectId: "moviecatalog-5b6c8",
+  storageBucket: "moviecatalog-5b6c8.appspot.com",
+  messagingSenderId: "718429604244",
+  appId: "1:718429604244:web:3bf631c02f3546afd72781"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
